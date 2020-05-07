47ece6b9402181eb8aaab466ecf440bb425ef872
mantal <mantal0@outlook.fr>
2020-05-07 16:13:13 +0200

    Fix FunctionService not registered to DI

--- RPG.Engine/Services/ServiceExtensions.cs
+++ RPG.Engine/Services/ServiceExtensions.cs
@@ -7,6 +7,7 @@ namespace RPG.Engine.Services
 		public static IServiceCollection AddRpgEngine(this IServiceCollection services)
 		{
 			services.AddSingleton<StatService>();
+			services.AddSingleton<FunctionService>();
 
 			return services;
 		}
