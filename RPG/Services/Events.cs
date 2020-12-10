8d4be661bb584c0112f4b6eb6fa43e974d51bf18
mantal <mantal0@outlook.fr>
2020-12-10 17:24:38 +0100

    Gearos: fix xp cost on load

---
+++ RPG/Services/Events.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RPG.Services
+{
+    public class Events
+	{
+		public event EventHandler? SheetUpdated;
+
+		public virtual void OnSheetUpdated()
+		{
+			SheetUpdated?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
--- RPG/Startup.cs
+++ RPG/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RPG.Data;
 using RPG.Engine.Services;
+using RPG.Services;
 
 namespace RPG
 {
@@ -40,6 +41,7 @@ namespace RPG
 			services.AddRpgEngine();
 
 			services.AddSingleton<Chat>();
+			services.AddSingleton<Events>();
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
